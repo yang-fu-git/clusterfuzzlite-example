51ff8d3e75e84a0d586c3f9f798bd07240e6c62b
Yang Fu <st180586@stud.uni-stuttgart.de>
2023-06-12 00:26:34 +0200

    Edit do another stuff

--- do_another_stuff.cpp
+++ do_another_stuff.cpp
@@ -3,23 +3,12 @@
 
 // Implementation of "my_api".
 #include "my_api.h"
-
+#include <string>
 #include <vector>
 
 // Do some computations with 'str', return the result.
 // This function contains a bug. Can you spot it?
 size_t DoAnotherStuff(const std::string &str) {
-  std::vector<int> Vec({0, 1, 2, 3, 4});
-  size_t Idx = 0;
-  if (str.size() > 5)
-    Idx++;
-  if (str.find("foo") != std::string::npos)
-    Idx++;
-  if (str.find("bar") != std::string::npos)
-    Idx++;
-  if (str.find("ouch") != std::string::npos)
-    Idx++;
-  if (str.find("omg") != std::string::npos)
-    Idx++;
-  return Vec[Idx];
+  std::string augument_string = str+str;
+  DoStuff(augument_string);
 }
