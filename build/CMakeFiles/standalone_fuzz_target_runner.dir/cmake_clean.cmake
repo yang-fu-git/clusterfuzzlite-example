65d1480a0fa50787e23b5c8579b0cf6c5c055f68
Yang Fu <st180586@stud.uni-stuttgart.de>
2023-09-25 10:34:19 +0200

    add cmakelists

---
+++ build/CMakeFiles/standalone_fuzz_target_runner.dir/cmake_clean.cmake
@@ -0,0 +1,9 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/standalone_fuzz_target_runner.dir/standalone_fuzz_target_runner.cpp.o"
+  "CMakeFiles/standalone_fuzz_target_runner.dir/standalone_fuzz_target_runner.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/standalone_fuzz_target_runner.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/standalone_fuzz_target_runner.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for standalone_fuzz_target_runner.
+# This may be replaced when dependencies are built.
