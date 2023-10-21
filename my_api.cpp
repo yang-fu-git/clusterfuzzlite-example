0d8f4526ec165237ddf9460ba717d7ce747bd17c
Yang Fu <st180586@stud.uni-stuttgart.de>
2023-10-21 10:50:19 +0200

    Edit doStuff

--- my_api.cpp
+++ my_api.cpp
@@ -19,9 +19,7 @@ size_t DoStuff(const std::string &str) {
     Idx++;
   if (str.find("ouch") != std::string::npos)
     Idx++;
-  if (str.find("om") != std::string::npos)
-    Idx++;
-  if (str.find("on") != std::string::npos)
+  if (str.find("omg") != std::string::npos)
     Idx++;
   return Vec[Idx];
 }
